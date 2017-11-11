48db62e35f12ac80c507ee946c3707e2038cbc54
Alex Prinsen <alexander.f.prinsen@gmail.com>
2017-11-10 23:53:40 -0600

    characters will be dumb containers

--- src/core/components/char.py
+++ src/core/components/char.py
@@ -9,16 +9,3 @@ class Character():
         self.health = health
         self.effects = {}
         self.__dict__.update(kwargs)
-
-    def attack(self, o, attack):
-        o.take_damage(attack)
-
-    def take_damage(self, attack):
-        multiplier = 1
-        for t in attack.types:
-            for i in self.interests:
-                if t in STRONG_AGAINST[i]:
-                    multiplier *= 2.0
-                if t in WEAK_AGAINST[i]:
-                    multiplier *= 0.5
-        self.health -= attack.base_damage * multiplier

13c5d8c8fcba4e014074ebb6e15fff17dc6149f8
Alex Prinsen <alexander.f.prinsen@gmail.com>
2017-11-12 23:27:49 -0600

    some design stuff

---
+++ src/core/encounter/encounter.py
@@ -0,0 +1,20 @@
+"""
+A friendly encounter takes place between two posses.
+
+An encounter is composed of one or more conversations.
+
+What do we want to track during an encounter -- ?
+
+current conversants
+health of each posse member
+
+
+rules --
+
+a conversation ends when one of the conversants hits zero health or are otherwise unable to continue
+a conversation ends when one of the conversants chooses to leave
+
+a friendly encounter ends when all members of one posse are unable to converse
+a friendly encounter ends when one of the posse's chooses to leave
+"""
+
